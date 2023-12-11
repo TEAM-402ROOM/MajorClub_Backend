47012d41f19c93cebf1fef89904b4c1f5db5a482
NameIsUser06 <woosoungcho@gmail.com>
2023-12-12 01:50:18 +0900

    feat : report get

--- src/main/java/com/major/club/domain/report/presentation/ReportController.java
+++ src/main/java/com/major/club/domain/report/presentation/ReportController.java
@@ -1,11 +1,15 @@
 package com.major.club.domain.report.presentation;
 
 import com.major.club.domain.report.presentation.dto.request.ReportRequest;
+import com.major.club.domain.report.presentation.dto.response.ReportResponse;
+import com.major.club.domain.report.service.GetReportListService;
 import com.major.club.domain.report.service.PostReportService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,9 +17,15 @@ import org.springframework.web.bind.annotation.*;
 public class ReportController {
 
     private final PostReportService postReportService;
+    private final GetReportListService getReportListService;
 
     @PostMapping
     public ResponseEntity<String> postReport(@RequestBody ReportRequest request, HttpServletRequest httpServletRequest) {
         return postReportService.execute(request, httpServletRequest);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ReportResponse>> getReportList(HttpServletRequest request) {
+        return getReportListService.execute(request);
     }
 }
--- src/main/java/com/major/club/domain/report/repository/ReportRepository.java
+++ src/main/java/com/major/club/domain/report/repository/ReportRepository.java
@@ -1,9 +1,13 @@
 package com.major.club.domain.report.repository;
 
+import com.major.club.domain.club.domain.Club;
 import com.major.club.domain.report.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findAllByClub(Club club);
 }
