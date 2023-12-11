595a92b7fd4bcbfad944b73c2bd0188947ce4bd0
NameIsUser06 <woosoungcho@gmail.com>
2023-12-12 00:30:43 +0900

    feat : club get

--- src/main/java/com/major/club/domain/club/presentation/ClubController.java
+++ src/main/java/com/major/club/domain/club/presentation/ClubController.java
@@ -1,12 +1,16 @@
 package com.major.club.domain.club.presentation;
 
 import com.major.club.domain.club.presentation.dto.request.ClubRequest;
+import com.major.club.domain.club.presentation.dto.response.ClubResponse;
+import com.major.club.domain.club.service.GetClubListService;
 import com.major.club.domain.club.service.JoinClubService;
 import com.major.club.domain.club.service.MakeClubService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +19,7 @@ public class ClubController {
 
     private final MakeClubService makeClubService;
     private final JoinClubService joinClubService;
+    private final GetClubListService getClubListService;
 
     @PostMapping("/make")
     public ResponseEntity<String> makeClub(@RequestBody ClubRequest postClubRequest) {
@@ -24,6 +29,11 @@ public class ClubController {
     @PostMapping("/join")
     public ResponseEntity<String> joinClub(@RequestBody ClubRequest clubRequest, HttpServletRequest request) {
         return joinClubService.execute(clubRequest, request);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<ClubResponse>> getClubList() {
+        return getClubListService.execute();
     }
 
 }
